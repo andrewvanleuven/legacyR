20d5bf481e5e132839cace9c8f63c328c041d064
Andrew Van Leuven <a@andrewvanleuven.com>
2019-12-24 00:35:02 -0500

    Housing value & age of housing stock variables

--- code/base/acs.R
+++ code/base/acs.R
@@ -17,6 +17,7 @@ univ <- read_csv("data/clustrdata.csv") %>%
   rename(cbsa_fips = id)
 
 
+# GINI --------------------------------------------------------------------
 gini <- get_acs(geography = "metropolitan statistical area/micropolitan statistical area", 
                 variables = c(gini_coeff = "B19083_001")) %>% 
   mutate(cbsa_fips = as.numeric(GEOID),
@@ -26,4 +27,22 @@ gini <- get_acs(geography = "metropolitan statistical area/micropolitan statisti
   select(1,3,2) %>% 
   write_csv("data/base/generated/gini.csv")
 
+
+# Median Value CBSA -------------------------------------------------------
+### B25077.  Median Value (Dollars) for Owner-Occupied Housing Units
+
+h_value_city <- read_csv("data/base/source/housing_value.csv")
+
+med_val_cbsa <- get_acs(geography = "metropolitan statistical area/micropolitan statistical area", 
+                variables = c(gini_coeff = "B25077_001")) %>% 
+  mutate(cbsa_fips = as.numeric(GEOID),
+         median_value_msa = estimate) %>% 
+  select(cbsa_fips,median_value_msa) %>% 
+  inner_join(.,univ) %>% 
+  select(1,3,2) %>% 
+  left_join(.,h_value_city) %>% 
+  select(cbsa_fips,cbsa,city_fips,city,median_value_msa,median_value) %>% 
+  rename(med_val_cbsa = median_value_msa,
+         med_val_city = median_value) %>% 
+  write_csv("data/base/generated/housing_value.csv")
 
--- code/base/census.R
+++ code/base/census.R
@@ -8,7 +8,8 @@ options(tigris_class = "sf")
 options(tigris_use_cache = TRUE)
 options(scipen = 999,"digits"=3)
 
-v00 <- load_variables(2000, "sf1", cache = T)
+vsf1 <- load_variables(2000, "sf1", cache = T)
+vsf3 <- load_variables(2000, "sf3", cache = T)
 us <- states(cb = TRUE, resolution = "20m") %>%
   filter(!STUSPS %in% c("PR")) %>% pull(STUSPS)
 xw <- read_csv("data/xw.csv") 
@@ -17,6 +18,17 @@ msaxw <- xw %>% select(cbsa_fips,cbsa,cbsa_type) %>%
   group_by(cbsa) %>% 
   summarise(cbsa_fips = mean(cbsa_fips)) %>% 
   filter(!str_detect(cbsa, ', PR'))
+city_pop <- get_decennial(year = 2000, variables = "P001001", state = us, geography = "place") %>% 
+  rename(city_fips = GEOID, city = NAME, pop = value) %>% select(city_fips,city,pop)
+cxw <- read_csv("data/city_xw.csv") %>% 
+  filter(cbsa_type == "Metropolitan Statistical Area",
+         !str_detect(cbsa, ', PR')) %>%
+  mutate(city_fips = str_pad(city_fips,7,"left","0")) %>% 
+  select(-city) %>% 
+  left_join(.,city_pop,by = "city_fips") %>% 
+  group_by(cbsa_fips) %>% 
+  filter(pop == max(pop))%>% 
+  select(-cbsa) 
 
 # Vacancy Rate ------------------------------------------------------------
 houses <- get_decennial(year = 2000, variables = "H001001", state = us, geography = "county") 
@@ -31,19 +43,6 @@ vacant <- get_decennial(year = 2000, variables = c("H001001","H005001"), state =
   mutate(vacancy = total_vacant_2000/total_units_2000) %>% 
   select(cbsa_fips,cbsa,total_vacant_2000,total_units_2000,vacancy) %>% 
   write_csv("data/base/generated/vacancy_msa.csv")
-
-city_pop <- get_decennial(year = 2000, variables = "P001001", state = us, geography = "place") %>% 
-  rename(city_fips = GEOID, city = NAME, pop = value) %>% select(city_fips,city,pop)
-
-cxw <- read_csv("data/city_xw.csv") %>% 
-  filter(cbsa_type == "Metropolitan Statistical Area",
-         !str_detect(cbsa, ', PR')) %>%
-  mutate(city_fips = str_pad(city_fips,7,"left","0")) %>% 
-  select(-city) %>% 
-  left_join(.,city_pop,by = "city_fips") %>% 
-  group_by(cbsa_fips) %>% 
-  filter(pop == max(pop))%>% 
-  select(-cbsa) 
 
 vacanc <- get_decennial(year = 2000, variables = c("H001001","H005001"), state = us, geography = "place") %>% 
   pivot_wider(names_from = variable) %>% 
@@ -163,5 +162,51 @@ nativity <- get_decennial(year = 2000,
          nativity = not_foreign_born/total) %>% 
   select(cbsa_fips,cbsa,total,foreign_born,not_foreign_born,native,nativity) %>% 
   write_csv("data/base/generated/nativity.csv")
+
+# Housing Stock Age -------------------------------------------------------
+# H034010	Total:  Built 1939 or earlier	(Year Structure Built)
+housing_stock <- get_decennial(year = 2000,
+                         variables = paste0("H0",34001:34010), 
+                         state = us, 
+                         geography = "county") %>% 
+  pivot_wider(names_from = variable) %>% 
+  rename(cty_fips = GEOID) %>% 
+  left_join(.,xw) %>% 
+  group_by(cbsa_fips) %>% 
+  summarize(total_prewar = sum(H034010),
+            total = sum(H034001)) %>% 
+  inner_join(.,msaxw) %>% 
+  mutate(pct_prewar_msa = total_prewar/total) %>% 
+  select(cbsa_fips,pct_prewar_msa)
+
+h_stock_city <- get_decennial(year = 2000,
+                               variables = c("H034001","H034010"),
+                               state = us, 
+                               geography = "place") %>%
+  pivot_wider(names_from = variable) %>% 
+  rename(city_fips = GEOID,
+         total = H034001,
+         total_prewar = H034010) %>% 
+  inner_join(cxw,., by = "city_fips") %>% 
+  inner_join(.,msaxw, by = "cbsa_fips") %>% 
+  mutate(pct_prewar_city = total_prewar/total) %>% 
+  select(cbsa_fips,cbsa,city_fips,city,pct_prewar_city) %>% 
+  left_join(.,housing_stock,by = "cbsa_fips") %>% 
+  write_csv("data/base/generated/prewar_housing_stock.csv")
+
+# Housing Value -------------------------------------------------------
+# H085001 Median Value (Dollars) for All Owner-occupied Housing Units 
+housing_value <- get_decennial(year = 2000,
+                               variables = "H085001", 
+                               state = us, 
+                               geography = "place") %>% 
+  rename(city_fips = GEOID,
+         median_value = value)
+
+
+h_value_city <- inner_join(cxw,housing_value, by = "city_fips") %>% 
+  inner_join(.,msaxw, by = "cbsa_fips") %>% 
+  select(cbsa_fips,cbsa,city_fips,city,median_value) %>% 
+  write_csv("data/base/source/housing_value.csv")
 
 

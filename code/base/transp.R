c80705f998d9c7af5da52d9efa2e4661099eff2c
Andrew Van Leuven <a@andrewvanleuven.com>
2019-12-24 00:01:25 -0500

    Added intermodal freight variable

---
+++ code/base/transp.R
@@ -0,0 +1,55 @@
+library(tidyverse)
+library(tidycensus)
+library(sf)
+library(tigris)
+library(rleuven)
+library(lubridate)
+library(ggpubr)
+options(tigris_class = "sf")
+options(tigris_use_cache = TRUE)
+options(scipen = 999,"digits"=3)
+
+# Setup the Universe ------------------------------------------------------
+univ <- read_csv("data/clustrdata.csv") %>% 
+  select(1:2) %>% rename_all(tolower) %>% 
+  rename(cbsa_fips = id)
+cbsa <- core_based_statistical_areas(cb = T) %>% 
+  select(GEOID:geometry) %>% 
+  mutate(cbsa_fips = as.numeric(GEOID)) %>% 
+  select(cbsa_fips,geometry) %>% 
+  inner_join(.,univ) %>% 
+  arrange(cbsa_fips) %>% 
+  st_transform(crs = 2163) %>% 
+  st_centroid_xy()
+
+### SOURCE: https://www.bts.gov/browse-statistical-products-and-data
+freight <- read_csv("data/base/source/freight.csv") %>% rename_all(tolower) %>% 
+  select(name,mode_type,longitude,latitude) %>% 
+  fastDummies::dummy_cols(., select_columns = "mode_type") %>%
+  rename_all(~stringr::str_replace_all(., "mode_type_", "")) %>% 
+  janitor::clean_names() %>% 
+  st_as_sf(.,coords = c("longitude", "latitude"), crs = 4326) %>% 
+  st_transform(crs = 2163) %>% 
+  st_intersection(cbsa,.) %>% 
+  select(-(name:mode_type)) %>% 
+  st_drop_geometry() %>% 
+  group_by(cbsa_fips) %>% 
+  summarize(intermodal_freight = n(),
+            n_rail_truck = sum(rail_truck),
+            n_truck_port_rail = sum(truck_port_rail),
+            n_port_truck = sum(port_truck),
+            n_truck_port_rail_air = sum(truck_port_rail_air),
+            n_rail_port = sum(rail_port),
+            n_truck_port_air = sum(truck_port_air),
+            n_air_truck = sum(air_truck),
+            n_truck_truck = sum(truck_truck),
+            n_truck_air_rail = sum(truck_air_rail)) %>% 
+  left_join(.,univ) %>% 
+  select(cbsa_fips,name,intermodal_freight,everything()) %>% 
+  write_csv("data/base/generated/freight.csv")
+
+
+  
+
+
+ipcd <- read_csv("data/base/source/ipcd.csv") %>% rename_all(tolower)
---
+++ univ.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(tidycensus)
+library(sf)
+library(tigris)
+library(rleuven)
+library(ggpubr)
+options(tigris_class = "sf")
+options(tigris_use_cache = TRUE)
+options(scipen = 999,"digits"=3)
+
+xw <- read_csv("data/xw.csv") 
+msaxw <- xw %>% select(cbsa_fips,cbsa,cbsa_type) %>% 
+  filter(cbsa_type == "Metropolitan Statistical Area") %>% 
+  group_by(cbsa) %>% 
+  summarise(cbsa_fips = mean(cbsa_fips)) %>% 
+  filter(!str_detect(cbsa, ', PR'))
+
+### SOURCE: https://nces.ed.gov/ipeds/datacenter/DataFiles.aspx
+zipxw <- read_csv("data/base/source/zip_xw.csv") %>% rename_all(tolower) %>% 
+  mutate(zip = as.numeric(zip)) %>% 
+  filter(tot_ratio > 0.5) %>% 
+  rename(cbsa_fips = cbsa) %>% 
+  select(1:2)
+
+universities <- read_csv("data/base/source/univ.csv") %>% 
+  mutate(zip = as.numeric(zip),
+         r1 = if_else(ccbasic == 15,1,0),
+         r2 = if_else(ccbasic == 16,1,0)) %>% 
+  select(-enrtot) %>% 
+  filter(r1 == 1 | r2 == 1,
+         !str_detect(st, 'PR')) %>% 
+  left_join(.,zipxw) %>% 
+  filter(!is.na(cbsa_fips)) %>% 
+  group_by(cbsa_fips) %>% 
+  summarise(r_1 = sum(r1),
+            r_2 = sum(r2)) %>% 
+  left_join(.,msaxw) %>% 
+  select(1,4,2,3) %>% 
+  write_csv("data/base/generated/univ.csv")
+  
+  
+
+  
